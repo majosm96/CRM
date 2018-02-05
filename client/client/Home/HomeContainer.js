f193a95883a1c1b9628dd17587198b5d6f9ac706
Maria Jose Solis <majosm96@gmail.com>
2018-02-05 10:00:16 -0600

    New HomeContainer

---
+++ client/client/Home/HomeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeContainer = () => (
+  <div>
+    <h3>New client</h3>
+    <div>
+      <label htmlFor="clientName">from:</label>
+      <input type="text" id="name" name="clientName" />
+    </div>
+  </div>
+);
+
+
+export default HomeContainer;
